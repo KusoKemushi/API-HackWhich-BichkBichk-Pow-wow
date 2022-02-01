33dcc0980c094f2d3db1adbae1fc66fb9fdc96e6
Kemushi <tigeramano@gmail.com>
2022-02-01 10:33:29 -0600

    Initial Commit

---
+++ API HackWhich BichkBichk Pow wow/API_HackWhich_BichkBichk_Pow_wowApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_HackWhich_BichkBichk_Pow_wowApp.swift
+//  API HackWhich BichkBichk Pow wow
+//
+//  Created by Student on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct API_HackWhich_BichkBichk_Pow_wowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
